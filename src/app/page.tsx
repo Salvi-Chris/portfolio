60849d5ef00ec35955772bc344535d90d2c3e103
calebbenjin <calebbenji0@gmail.com>
2025-06-03 23:31:56 +0100

    feat: Salvi portfolio content details

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Footer from "@/components/Footer";
 import IntroSection from "@/components/IntroSection";
 import ProjectSections from "@/components/ProjectSections";
-import UserProfile from "@/components/UserProfile";
 import WorkTogetherSection from "@/components/WorkTogetherSection";
 
 export default function Home() {
